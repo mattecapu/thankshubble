1698acdf686c8fac12cfe823b4b991db62fa368a
Matteo Capucci <mattecapu@live.it>
2015-04-22 14:31:01 +0200

    working code, more es6/es7!

--- arxiv_api.js
+++ arxiv_api.js
@@ -1,19 +1,36 @@
-import { qs as querystring } from 'querystring';
+import qs from 'querystring';
+import Promise from 'bluebird';
 import request from 'request';
 import { parseString as xml } from 'xml2js';
 
-const arxiv_query = {
-	search_query: 'all:("hubble telescope")',
-	start: 0,
-	max_results: 50,
-	sortBy: 'lastUpdatedDate'
-}
-const arxiv_api_url = 'http://export.arxiv.org/api/query?' + qs.stringify(arxiv_query);
+xml = Promise.promisify(xml);
+request = Promise.promisify(request);
 
-export default function (res) {
-	request(arxiv_api_url, (err, response, body) => {
-		if (err) res.send('sorry! something went wrong. try to reload the page.');
-		//const data = await xml(body);
-		res.send(body);
-	});
+export default async function (res) {
+	const arxiv_query = {
+		search_query: 'all:("hubble telescope")',
+		start: 0,
+		max_results: 50,
+		sortBy: 'lastUpdatedDate'
+	}
+	const arxiv_api_url = 'http://export.arxiv.org/api/query?' + qs.stringify(arxiv_query);
+	
+	try {
+		// make the request and wait for the result
+		let [, body] = await request(arxiv_api_url);
+		// fetch the papers list
+		let papers = (await xml(body)).feed.entry;
+
+		// filter the info and return the new collection
+		return papers.map((paper) => {
+			return {
+				title: paper.title,
+				abstract: paper.summary,
+				authors: paper.author.map(x => x.name),
+				href: paper.link[0].$.href
+			}
+		})
+	} catch(e) {
+		return 'sorry! something went wrong. try to reload the page.';
+	}
 }
