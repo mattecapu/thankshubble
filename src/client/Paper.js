a2a79634bee5774c8c5acd270e3c19b626aecd97
Matteo Capucci <mattecapu@live.it>
2015-04-23 16:04:39 +0200

    removed useless contructor, added target="blank" to links

--- src/client/Paper.js
+++ src/client/Paper.js
@@ -2,20 +2,16 @@ import $ from 'jquery';
 import React from 'react';
 
 export default class Paper extends React.Component {
-	constructor(props) {
-		super(props);
-		console.log(this.props);
-	};
 	render = () => {
 		return (
 			<article className="paper">
 				<h2 className="paper__title">
-					<a href={this.props.href}>{this.props.title}</a>
+					<a href={this.props.href} target="_blank">{this.props.title}</a>
 				</h2>
 				<p className="paper__abstract">{this.props.abstract}</p>
 				<footer>
 					<ul className="authors">
-						{this.props.authors.map(x => <li>x</li>)}
+						{this.props.authors.map(x => <li>{x}</li>)}
 					</ul>
 				</footer>
 			</article>
